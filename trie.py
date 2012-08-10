d46cc490e093a6d3857db8ad57e7992ad14b1949
Michael <mpchoy@gmail.com>
2012-08-10 11:30:09 -0700

    Can insert and retrieve basic strings.

--- trie.py
+++ trie.py
@@ -1,14 +1,37 @@
 class Node:
     """A Trie node"""
     def __init__( self, key ):
-        self.key = key
-        self.children = []
+        self.key = key # TODO remove
+        self.children = {}
+        self.data = None
 
     def __str__( self ):
         print "<" + self.key + ">"
 
 
 class Trie:
-    """A simple trie structure"""
+    """
+    A simple trie structure
+    Keys must be orderable.
+    """
     def __init__( self ):
-        self.root = None
+        self.root = Node( '' )
+
+    def insert( self, key, data ):
+        node = self.root
+        i = 0
+        for k in key:
+            if k not in node.children:
+                node.children[k] = Node( k )
+            node = node.children[k]
+        node.data = data
+
+    def get( self, key ):
+        node = self.root
+
+        for k in key:
+            if k not in node.children:
+                return None
+            node = node.children[k]
+
+        return node.data
--- trie_test.py
+++ trie_test.py
@@ -10,5 +10,26 @@ class TestTrie(unittest.TestCase):
         self.assertEqual(node.key, 'a')
         self.assertEqual(len(node.children), 0)
 
+    def test_init(self):
+        trie = Trie()
+
+    def test_insert_on_empty(self):
+        trie = Trie()
+        data = 'is for apple'
+        trie.insert( 'a', data )
+        self.assertIsNotNone(trie.get('a'))
+        self.assertEqual(trie.get('a'), data)
+
+    def test_insert_on_empty_long(self):
+        trie = Trie()
+        data = 'ripper X!'
+        trie.insert( 'ab', data )
+        self.assertIsNone(trie.get('a'))     # a is not in the dict
+        self.assertIsNotNone(trie.get('ab')) # ab is
+        self.assertEqual(trie.get('ab'), data)
+
+    # TODO don't forget to try numbers
+
+
 if __name__ == '__main__':
     unittest.main()
