01cb80b2c43b9d8d6c426a5d426a3523b737fdc3
Michael <mpchoy@gmail.com>
2012-08-10 11:38:31 -0700

    Same code, more tests

--- trie_test.py
+++ trie_test.py
@@ -13,6 +13,12 @@ class TestTrie(unittest.TestCase):
     def test_init(self):
         trie = Trie()
 
+    def test_get_on_empty(self):
+        trie = Trie()
+        self.assertIsNone(trie.get('a'))
+        self.assertIsNone(trie.get('ab'))
+        self.assertIsNone(trie.get('xyz0'))
+
     def test_insert_on_empty(self):
         trie = Trie()
         data = 'is for apple'
@@ -27,6 +33,25 @@ class TestTrie(unittest.TestCase):
         self.assertIsNone(trie.get('a'))     # a is not in the dict
         self.assertIsNotNone(trie.get('ab')) # ab is
         self.assertEqual(trie.get('ab'), data)
+
+    def test_insert_on_empty_longer(self):
+        trie = Trie()
+        key = 'abstentia'
+        data = 'Must be present to win'
+        trie.insert( key, data )
+        self.assertIsNone(trie.get('a'))
+        self.assertIsNone(trie.get('absent'))
+        self.assertEqual(trie.get(key), data)
+
+    def test_insert_fork(self):
+        trie = Trie()
+        ab_data = 'ripper X!'
+        ax_data = 'for chopping'
+        trie.insert( 'ab', ab_data )
+        trie.insert( 'ax', ax_data )
+        self.assertIsNone(trie.get('a'))
+        self.assertEqual(trie.get('ab'), ab_data)
+        self.assertEqual(trie.get('ax'), ax_data)
 
     # TODO don't forget to try numbers
 
